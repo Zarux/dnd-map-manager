31cfa1f145c2b4138a2bc14fd18f233bff41dbdb
Joakim <joakimtangnes@gmail.com>
2017-06-10 01:42:03 +0200

    fix packages

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -12,11 +12,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,         // Match both .js and .jsx files
                 exclude: /node_modules/,
-                loader: "babel-loader",
-                query:
-                    {
-                        presets:['react']
-                    }
+                loader: "babel-loader"
             },
             {
                 test: /\.scss$/,
