42a88f24e65047ab6719086e2640571b26552948
Joakim <joakimtangnes@gmail.com>
2017-06-10 16:43:05 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 mkdir server/images/full && echo "Made full image directory"
 mkdir server/images/thumbs && echo "Made thumbnail directory"
-printf "module.exports = {\nredisPassword: '',\nredisHost: ''\nserverPort:9001\n};" > server/config.js && echo "Made server/config.js"
+printf "module.exports = {\nredisPassword: '',\nredisHost: '',\nserverPort: 9001\n};\n" > server/config.js && echo "Made server/config.js"
 yarn
 if gm > /dev/null 2>&1  ; then
     echo "You have GraphicksMagick installed (i think)"
