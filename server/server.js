a50ef87eb9cd19c5f8aa667aaca631326c25c8a6
Joakim <joakimtangnes@gmail.com>
2017-06-10 02:17:36 +0200

    fix path

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ io.sockets.on('connection', function (socket) {
     socket.on("join-room", data => {
         socket.join(data.room);
         socket.mainRoom = data.room;
-        const fileDir = `${__dirname}\\images\\full\\${socket.mainRoom}`;
-        const thumbDir = `${__dirname}\\images\\thumbs\\${socket.mainRoom}`;
+        const fileDir = `${__dirname}/images/full/${socket.mainRoom}`;
+        const thumbDir = `${__dirname}/images/thumbs/${socket.mainRoom}`;
         if (!fs.existsSync(fileDir)){
             fs.mkdirSync(fileDir);
         }
@@ -27,9 +27,9 @@ io.sockets.on('connection', function (socket) {
     socket.on("save-images", data => {
         if(!data.files) return;
         Array.from(data.files).forEach((file) => {
-            const fileDir = `${__dirname}\\images\\full\\${socket.mainRoom}`;
-            const thumbDir = `${__dirname}\\images\\thumbs\\${socket.mainRoom}`;
-            const fileName = `${fileDir}\\${file.name}`;
+            const fileDir = `${__dirname}/images/full/${socket.mainRoom}`;
+            const thumbDir = `${__dirname}/images/thumbs/${socket.mainRoom}`;
+            const fileName = `${fileDir}/${file.name}`;
             if (!fs.existsSync(fileDir)){
                 fs.mkdirSync(fileDir);
             }
@@ -51,17 +51,17 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on("get-thumbnails", data => {
-        const thumbDir = `${__dirname}\\images\\thumbs\\${socket.mainRoom}`;
+        const thumbDir = `${__dirname}/images/thumbs/${socket.mainRoom}`;
         const files = [
-            {name:"white-100.png", image: fs.readFileSync(`${__dirname}\\images\\white.png`).toString('base64')}
+            {name:"white-100.png", image: fs.readFileSync(`${__dirname}/images/white.png`).toString('base64')}
         ];
         fs.readdir(thumbDir, (err, filenames) => {
             if (err) throw err;
             filenames.sort((a, b) => {
-                return fs.statSync(`${thumbDir}\\${a}`).mtime.getTime() + fs.statSync(`${thumbDir}\\${b}`).mtime.getTime();
+                return fs.statSync(`${thumbDir}/${a}`).mtime.getTime() + fs.statSync(`${thumbDir}/${b}`).mtime.getTime();
             });
             for(let i = 0; i < filenames.length; i++){
-                const content = fs.readFileSync(`${thumbDir}\\${filenames[i]}`);
+                const content = fs.readFileSync(`${thumbDir}/${filenames[i]}`);
                 files.push({name: filenames[i], image: content.toString('base64')})
             }
             socket.emit("thumbnails", {
@@ -72,12 +72,12 @@ io.sockets.on('connection', function (socket) {
 
     socket.on("get-full-image", data => {
         let fileName = data.name.replace(/(.*)-100\.(.*)/,"$1.$2");
-        const fileDir = `${__dirname}\\images\\full\\${socket.mainRoom}`;
-        const thumbDir = `${__dirname}\\images\\thumbs\\${socket.mainRoom}`;
+        const fileDir = `${__dirname}/images/full/${socket.mainRoom}`;
+        const thumbDir = `${__dirname}/images/thumbs/${socket.mainRoom}`;
         if(fileName === "white.png"){
            fileName = "../../white.png"
         }
-        fs.readFile(`${fileDir}\\${fileName}`, (err, data) => {
+        fs.readFile(`${fileDir}/${fileName}`, (err, data) => {
             if (err) throw err;
             socket.emit("full-image", {
                 image: data.toString('base64')
